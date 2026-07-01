b0b5c4cf1a5dfda7fca48a4b2bc45a3b15db629f
ajayajay17049 <cvvijaykumar.2013@gmail.com>
2026-07-01 19:56:16 +0530

    Add files via upload

---
+++ seticon.py
@@ -0,0 +1,10 @@
+# 1. To set icon go to faticon website:
+# 2. convert to ico format website_icon convertor:
+
+import tkinter
+window=tkinter.Tk()
+window.geometry("9000x9600")
+window.title("....## Tkinter File ##.....")
+window.config(bg="Purple")
+window.inconbitmap("favicon.ico")
+window.mainloop()
